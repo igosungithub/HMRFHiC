72786d4ffd6fd343870f8052351b95dd23b8b743
igosungithub <85625734+igosungithub@users.noreply.github.com>
2024-03-21 18:48:12 +0000

    Update Result.R

--- Result.R
+++ Result.R
@@ -1,54 +1,67 @@
+mysample=cbind(sample_data$start.i., sample_data$end.j., sample_data$distance, sample_data$interaction)
+mysample=as.data.frame(mysample)
 
-myresult=cbind(sim_data$start.i., sim_data$end.j., sim_data$interaction)
-myresult=as.data.frame(myresult)
-names(myresult)=c("start.i.","end.j.","interaction")
+burnin=10000
+iterations=20000
 
-intercept_1=mean(chain_betas[[1]][[1]][burnin:iterations,1][])
-distance_1=mean(chain_betas[[1]][[1]][burnin:iterations,2])
-intercept_2=mean(chain_betas[[1]][[2]][burnin:iterations,1])
-distance_2=mean(chain_betas[[1]][[2]][burnin:iterations,2])
-intercept_3=mean(chain_betas[[1]][[3]][burnin:iterations,1])
-distance_3=mean(chain_betas[[1]][[3]][burnin:iterations,2])
-GC_1=mean(chain_betas[[1]][[1]][burnin:iterations,3])
-GC_2=mean(chain_betas[[1]][[2]][burnin:iterations,3])
-GC_3=mean(chain_betas[[1]][[3]][burnin:iterations,3])
-TES_1=mean(chain_betas[[1]][[1]][burnin:iterations,4])
-TES_2=mean(chain_betas[[1]][[2]][burnin:iterations,4])
-TES_3=mean(chain_betas[[1]][[3]][burnin:iterations,4])
-ACC_1=mean(chain_betas[[1]][[1]][burnin:iterations,5])
-ACC_2=mean(chain_betas[[1]][[2]][burnin:iterations,5])
-ACC_3=mean(chain_betas[[1]][[3]][burnin:iterations,5])
-
-
-y_datt=sim_data$interaction
-
-x1111=log(abs(sim_data$end.j.-sim_data$start.i.)+1)
-
-x2222=log(sim_data$GC+1)
-
-x3333=log(sim_data$Tes+1)
-
-x4444=log(sim_data$Acc+1)
+intercept_1=mean(chain_betas1[[1]][["chains"]][[1]][burnin:iterations,1])
+distance_1=mean(chain_betas1[[1]][["chains"]][[1]][burnin:iterations,2])
+intercept_2=mean(chain_betas1[[1]][["chains"]][[2]][burnin:iterations,1])
+distance_2=mean(chain_betas1[[1]][["chains"]][[2]][burnin:iterations,2])
+intercept_3=mean(chain_betas1[[1]][["chains"]][[3]][burnin:iterations,1])
+distance_3=mean(chain_betas1[[1]][["chains"]][[3]][burnin:iterations,2])
+GC_1=mean(chain_betas1[[1]][["chains"]][[1]][burnin:iterations,3])
+GC_2=mean(chain_betas1[[1]][["chains"]][[2]][burnin:iterations,3])
+GC_3=mean(chain_betas1[[1]][["chains"]][[3]][burnin:iterations,3])
+TES_1=mean(chain_betas1[[1]][["chains"]][[1]][burnin:iterations,4])
+TES_2=mean(chain_betas1[[1]][["chains"]][[2]][burnin:iterations,4])
+TES_3=mean(chain_betas1[[1]][["chains"]][[3]][burnin:iterations,4])
+ACC_1=mean(chain_betas1[[1]][["chains"]][[1]][burnin:iterations,5])
+ACC_2=mean(chain_betas1[[1]][["chains"]][[2]][burnin:iterations,5])
+ACC_3=mean(chain_betas1[[1]][["chains"]][[3]][burnin:iterations,5])
+theta=mean(chain_betas1[[1]][["theta"]][burnin:iterations])
 
 
-lambda_1<-exp(intercept_1 +(distance_1*x111) + (GC_1*x222) + (TES_1*x333) + (ACC_1*x444))
-lambda_2<-exp(intercept_2 +(distance_2*x111) + (GC_2*x222) + (TES_2*x333) + (ACC_2*x444))
-lambda_3<-exp(intercept_3 +(distance_3*x111) + (GC_3*x222) + (TES_3*x333) + (ACC_3*x444))
+lambda_1<-((intercept_1) +(distance_1)*(x111) + (GC_1)*(x222) + (TES_1)*(x333) + (ACC_1)*(x444))
+lambda_2<-((intercept_2) +(distance_2)*(x111) + (GC_2)*(x222) + (TES_2)*(x333) + (ACC_2)*(x444))
+lambda_3<-((intercept_3) +(distance_3)*(x111) + (GC_3)*(x222) + (TES_3)*(x333) + (ACC_3)*(x444))
+
+#####ZIP function for the output
+prob_zip <- function(y_dat, lambda, theta) {
+    # Initialize variable to store log probability
+    log_prob <- numeric(length(y_dat))
+    
+    # Calculate log probability for y_dat == 0
+    log_prob[y_dat == 0] <- log(theta + (1 - theta) * exp(dpois(0, lambda = exp(lambda[y_dat == 0]), log = TRUE)))
+    
+    # Calculate log probability for y_dat > 0
+    if (any(y_dat > 0)) {
+        log_prob[y_dat > 0] <- log(1 - theta) + dpois(y_dat[y_dat > 0], lambda = exp(lambda[y_dat > 0]), log = TRUE)
+    }
+    
+    return(log_prob)
+}
 
 
-prob1=dpois(y_dat,lambda_1)
-prob2=dpois(y_dat,lambda_2)
-prob3=dpois(y_dat,lambda_3)
+prob1=prob_zip(y_dat,lambda_1,theta)
+prob2=dpois(y_dat,exp(lambda_2),log=T)
+prob3=dpois(y_dat,exp(lambda_3),log=T)
 
 
 prob11=prob1/(prob1+prob2+prob3)
 prob22=prob2/(prob1+prob2+prob3)
 prob33=prob3/(prob1+prob2+prob3)
 
-myresult$prob2<-as.vector(prob22)
-myresult$prob2[is.na(myresult$prob2)]<-0
-myresult$prob3<-as.vector(prob33)
-myresult$prob3[is.na(myresult$prob3)]<-0
 
-myresult$Z2<-rep(0,nrow(myresult))
-myresult$Z2[myresult$prob2>=0.9]<-1  #####the probability can be changed either to a lower value or higher value to determine the certainty of the interacting pair 
+mysample$prob1<-as.vector(prob11)
+mysample$prob2<-as.vector(prob22)
+mysample$prob3<-as.vector(prob33)
+
+
+#####the probability can be changed either to a lower value or higher value to determine the certainty of the interacting pair 
+mysample$Z1<-rep(0,nrow(mysample))
+mysample$Z1[mysample$prob1>=0.5]<-1
+mysample$Z2<-rep(0,nrow(mysample))
+mysample$Z2[mysample$prob2>=0.5]<-1
+mysample$Z3<-rep(0,nrow(mysample))
+mysample$Z3[mysample$prob3>=0.5]<-1
