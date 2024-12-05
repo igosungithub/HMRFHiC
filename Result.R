5b5667d95189075a42827576b328b2d107bb941d
igosungithub <85625734+igosungithub@users.noreply.github.com>
2024-12-05 19:37:14 +0000

    Result.R

--- Result.R
+++ Result.R
@@ -1,5 +1,4 @@
-mysample=cbind(sample_data$start.i., sample_data$end.j., sample_data$distance, sample_data$interaction)
-mysample=as.data.frame(mysample)
+sample_data=as.data.frame(sample_data)
 
 burnin=(iterations/2)
 
@@ -21,29 +20,41 @@ ACC_3=mean(chain_betas1[[1]][["chains"]][[3]][burnin:iterations,5])
 theta=mean(chain_betas1[[1]][["theta"]][burnin:iterations])
 
 
-lambda_1<-((intercept_1) +(distance_1)*(x111) + (GC_1)*(x222) + (TES_1)*(x333) + (ACC_1)*(x444))
-lambda_2<-((intercept_2) +(distance_2)*(x111) + (GC_2)*(x222) + (TES_2)*(x333) + (ACC_2)*(x444))
-lambda_3<-((intercept_3) +(distance_3)*(x111) + (GC_3)*(x222) + (TES_3)*(x333) + (ACC_3)*(x444))
+lambda_1<-((intercept_1) +(distance_1)*log(x111+1) + (GC_1)*log(x222+1) + (TES_1)*log(x333+1) + (ACC_1)*log(x444+1))
+lambda_2<-((intercept_2) +(distance_2)*log(x111+1) + (GC_2)*log(x222+1) + (TES_2)*log(x333+1) + (ACC_2)*log(x444+1))
+lambda_3<-((intercept_3) +(distance_3)*log(x111+1) + (GC_3)*log(x222+1) + (TES_3)*log(x333+1) + (ACC_3)*log(x444+1))
 
-prob1=dpois(y_dat,exp(lambda_1),log=T)
+zip_prob <- function(y, lambda, theta) {
+  ifelse(
+    y == 0,
+    log(theta + (1 - theta) * exp(-lambda)),  # Zero inflation for y = 0
+    log(1 - theta) + dpois(y, lambda = lambda, log = TRUE)  # Poisson for y > 0
+  )
+}
+
+
+prob1=zip_prob(y_dat,exp(lambda_1),theta)
 prob2=dpois(y_dat,exp(lambda_2),log=T)
 prob3=dpois(y_dat,exp(lambda_3),log=T)
 
+prob1=exp(prob1)
+prob2=exp(prob2)
+prob3=exp(prob3)
 
 prob11=prob1/(prob1+prob2+prob3)
 prob22=prob2/(prob1+prob2+prob3)
 prob33=prob3/(prob1+prob2+prob3)
 
 
-mysample$prob1<-as.vector(prob11)
-mysample$prob2<-as.vector(prob22)
-mysample$prob3<-as.vector(prob33)
+sample_data$prob1<-as.vector(prob11)
+sample_data$prob2<-as.vector(prob22)
+sample_data$prob3<-as.vector(prob33)
 
 
 #####the probability can be changed either to a lower value or higher value to determine the certainty of the interacting pair 
-mysample$Z1<-rep(0,nrow(mysample))
-mysample$Z1[mysample$prob1>=0.5]<-1
-mysample$Z2<-rep(0,nrow(mysample))
-mysample$Z2[mysample$prob2>=0.5]<-1
-mysample$Z3<-rep(0,nrow(mysample))
-mysample$Z3[mysample$prob3>=0.5]<-1
+sample_data$Z1<-rep(0,nrow(sample_data))
+sample_data$Z1[sample_data$prob1>=0.5]<-1
+sample_data$Z2<-rep(0,nrow(sample_data))
+sample_data$Z2[sample_data$prob2>=0.5]<-1
+sample_data$Z3<-rep(0,nrow(sample_data))
+sample_data$Z3[sample_data$prob3>=0.5]<-1
